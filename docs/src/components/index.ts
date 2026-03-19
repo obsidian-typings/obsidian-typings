644670239839b917f3ff995cc244b2cc95e36b03
Michael Naumov <mnaoumov@gmail.com>
2026-03-19 17:45:14 -0600

    chore: remove docs

--- docs/src/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as LinkCardIcon } from './LinkCardIcon.astro';
--- docs/src/env.d.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import-x/no-unassigned-import, import-x/no-unresolved -- No other way.
-import '../.astro/types.d.ts';
-// eslint-disable-next-line import-x/no-unassigned-import -- No other way.
-import 'astro/client';
