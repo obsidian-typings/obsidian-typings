e5da0359c3293c3d3db7b9e3ecb64ca1005ce677
Michael Naumov <mnaoumov@gmail.com>
2026-03-23 13:59:13 -0600

    chore: don't require bun installed

--- scripts/release-beta.ts
+++ scripts/release-beta.ts
@@ -1,3 +1,3 @@
 import { execFromRoot } from './helpers/exec.ts';
 
-await execFromRoot('git show origin/main:workflow-scripts/release-beta.ts | bun -');
+await execFromRoot('git show origin/main:workflow-scripts/release-beta.ts | npx -y bun -');
